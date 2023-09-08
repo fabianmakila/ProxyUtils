cb594c2a39f8dd2e3f876c56533c9aadb6b83526
FabianAdrian <contact@fabianadrian.fi>
2023-09-08 07:14:21 +0300

    feat: remove base permission from subcommands

--- common/src/main/java/fi/fabianadrian/proxyutils/common/command/ProxyUtilsCommand.java
+++ common/src/main/java/fi/fabianadrian/proxyutils/common/command/ProxyUtilsCommand.java
@@ -7,30 +7,28 @@ import fi.fabianadrian.proxyutils.common.ProxyUtils;
 public abstract class ProxyUtilsCommand {
 	protected final ProxyUtils proxyUtils;
 	protected final CommandManager<Commander> manager;
-	private final String commandName;
-	private final String[] commandAliases;
 	private final String permission;
+	private final Command.Builder<Commander> builder;
 
 	public ProxyUtilsCommand(ProxyUtils proxyUtils, String commandName, String... commandAliases) {
 		this.proxyUtils = proxyUtils;
 		this.manager = proxyUtils.platform().commandManager();
 
-		this.commandName = commandName;
-		this.commandAliases = commandAliases;
-
 		this.permission = "proxyutils.command." + commandName;
+
+		this.builder = this.manager.commandBuilder(
+				commandName,
+				commandAliases
+		);
 	}
 
 	public abstract void register();
 
 	protected Command.Builder<Commander> builder() {
-		return this.manager.commandBuilder(
-				this.commandName,
-				this.commandAliases
-		).permission(this.permission);
+		return this.builder.permission(this.permission);
 	}
 
 	protected Command.Builder<Commander> subCommand(String literal) {
-		return this.builder().literal(literal).permission(this.permission + "." + literal);
+		return this.builder.literal(literal).permission(this.permission + "." + literal);
 	}
 }
