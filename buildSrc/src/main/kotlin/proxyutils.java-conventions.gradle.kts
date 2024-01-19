43d1e929259c0fa7b128ac03b88de0048ef4d6c0
FabianAdrian <contact@fabianadrian.fi>
2024-01-19 23:23:10 +0200

    feat: require java 17

--- buildSrc/src/main/kotlin/proxyutils.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/proxyutils.java-conventions.gradle.kts
@@ -7,7 +7,7 @@ group = rootProject.group
 version = rootProject.version
 description = rootProject.description
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(11))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
 tasks {
 	compileJava {
